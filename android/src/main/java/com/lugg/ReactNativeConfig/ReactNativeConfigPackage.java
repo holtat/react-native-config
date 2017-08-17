212d96f5d6399c333d5ba1c9e410d1faa2f90a76
Andrew Holt <andrew.holt@ey.com>
2017-08-17 13:48:20 -0400

    Fixes support for RN 46 and breaks RN 47.

--- android/src/main/java/com/lugg/ReactNativeConfig/ReactNativeConfigPackage.java
+++ android/src/main/java/com/lugg/ReactNativeConfig/ReactNativeConfigPackage.java
@@ -19,6 +19,11 @@ public class ReactNativeConfigPackage implements ReactPackage {
     }
 
     @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
